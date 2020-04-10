a7a3455bf05d5ba9e05f7814d7be2e12a286c508
melangyun <melangyun@gmail.com>
2020-04-10 23:10:14 +0900

    connect the database

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port:any = process.env.PORT;
+  await app.listen(port);
+  console.log(`app listen on : ${port}`)
+
+  
 }
 bootstrap();
