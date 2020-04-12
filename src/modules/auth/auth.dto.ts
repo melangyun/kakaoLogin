9b71f55e7d52ee8fca21e7020f62c97eedf02eb5
melangyun <melangyun@gmail.com>
2020-04-13 02:39:53 +0900

    add pipe and class-validate

--- src/modules/auth/auth.dto.ts
+++ src/modules/auth/auth.dto.ts
@@ -1,43 +1,54 @@
 import { ApiProperty } from "@nestjs/swagger";
-
+import { IsEmail, IsString, MinLength, MaxLength, IsDefined, Matches } from "class-validator";
+// class-validator 메소드 모음 : https://www.npmjs.com/package/class-validator
 export class SignUpDTO{
     @ApiProperty({
         description : "The Email for login (unique)",
         type: String,
     })
+    @IsEmail()
+    @IsDefined()
     readonly email: string;
 
     @ApiProperty({
         description : "The phone number",
         type: String
     })
+    @IsString()
+    @IsDefined()
+    @Matches( /^\d{3}-\d{3,4}-\d{4}$/)
     readonly phone:string;
 
     @ApiProperty({
         description : "The PW you want",
         type: String,
     })
+    @IsString()
+    @IsDefined()
+    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,16}/)
     readonly password: string;
 
     @ApiProperty({
         description : "The nickname you want",
         type: String,
     })
+    @IsString()
+    @IsDefined()
     readonly nickname: string;
 
     @ApiProperty({
         description: "Kakao AccessToken",
-        type: String,
+        type: String||null,
         nullable:true
     })
-    readonly kakaoAccessToken?: string;
+    readonly kakaoAccessToken?: string|null;
 
     @ApiProperty({
         description: "Kakao RefreshToken",
-        type: String,
+        type: String||null,
         nullable:true
     })
-    readonly kakaoRefreshToken?: string;
+    readonly kakaoRefreshToken?: string|null;
 }
 
 export class LoginDTO{
@@ -45,6 +56,9 @@ export class LoginDTO{
         description : "User Email",
         type: String,
     })
+    @IsEmail()
+    @IsString()
+    @IsDefined()
     readonly email: string;
 
     @ApiProperty({
@@ -59,11 +73,15 @@ export class KakaoInfoDTO{
         description : "kakao AccessToken",
         type: String,
     })
+    @IsString()
+    @IsDefined()
     readonly kakaoAccessToken: string;
 
     @ApiProperty({
         description : "kakao RefreshToken",
         type: String,
     })
+    @IsString()
+    @IsDefined()
     readonly kakaoRefreshToken: string;
 }
